05dc12763b1102f926868fdf4bd21d3edb2b1dc4
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2010-10-30 18:28:43 +0200

    Initial commit. Everything is so far working, only some flaw with parsing comments / counting them correctly. Shouldn't take long.

---
+++ Comment.py
@@ -0,0 +1,36 @@
+class Comment:
+    def __init__(self):
+        self.name = ""
+        self.helpful = ""
+        self.header = ""
+        self.comment = ""
+    def __repr__(self):
+        return "Name: %s\
+                Helpful: %s\
+                Header: %s\
+                Comment = %s"
+
+"""
+<!-- BOUNDARY -->
+<a name="R7FRJ2N28RVIZ"></a><br />
+<table>
+  <tr>
+    <td>
+
+      <div style="margin-bottom:0.5em;">
+        <div class="tiny">81 of 86 people found the following review helpful:</div>
+      </div>
+
+        <div>
+          <span><span class="swSprite s_star_1_0 " title="1.0 out of 5 stars" ><span>1.0 out of 5 stars</span></span> </span>
+          <b>Great Sound, build quality a joke</b><br />
+          I got these for xmas about a month ago. I've since used them about 5 times. This is my third pair of sennheisers, I also own the mx500 earbuds and the px100 headphones. Sennheiser has always had a great sound, and this set is no exception, wonderful for the price range, and the isolation of outside noise is great too.<br /><br />Thats where the good ends however. These...
+        </div>
+        <div class="tiny" style="margin-top:5px; clear: both;">
+          Published 21 months ago by D. Kruest
+        </div>
+
+    </td>
+  </tr>
+</table>
+"""
---
+++ amazoncommentfetcher.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from sys import argv, exit, stdout
+from strip_html import strip_ml_tags as stripHtmlTags
+from time import time
+
+from Comment import Comment
+
+comments = []
+
+def urlopener(url):
+    # TODO user agent randomizer
+    if "http://" in url:
+        import urllib
+
+        class MyOpener(urllib.FancyURLopener):
+            version = "Mozilla/5.0"
+        myopener = MyOpener()
+        try:
+            f = myopener.open(url)
+            contents = f.readlines()
+            f.close()
+            return contents
+        except:
+            print "Cannot fetch http data from %s" % url
+            return None
+    # Got file
+    else:
+        try:
+            O = open(url, "r")
+            contents = O.readlines()
+            O.close()
+            return contents
+        except:
+            print "Cannot open file %s" % str(url)
+            return None
+
+
+def getNextPageURL(data):
+    #<span class="paging">&lsaquo; Previous |    <span class="on">1</span> <a href="http://www.amazon.com/Introduction-Algorithms-Third-Thomas-Cormen/product-reviews/0262033844?pageNumber=2" >2</a>
+    #| <a href="http://www.amazon.com/Introduction-Algorithms-Third-Thomas-Cormen/product-reviews/0262033844?pageNumber=2" >Next &rsaquo;</a> </span>
+    for line in reversed(data):
+        if "<span class=\"paging\">" in line:
+            line = line.split("|")
+            line = line[::-1]
+            return parseLinkFromLine(line[0].split("Next")[0])
+    return None
+
+
+def parseReviewsStartLine(data):
+    """
+    @return (linenumber, link) when succeed
+    @return None in case of fail
+    """
+    # XXX *remove this line* searchPat = re.compile(r"See all (\d+) customer reviews")
+    # XXX *remove this line* searchPat = re.compile(r"See all ?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])? customer reviews")
+
+    # Introduction to re.compile line. Will find lines:
+    # "See all 14 customer reviews" or
+    # "See all 1,114 customer reviews"
+    links = {} # Use dictionary to avoid duplicates
+    searchPat = re.compile(r"See all (\d|,)*\d* customer reviews")
+    totalcomments = 0
+
+    if data is None or len(data) < 1:
+        return None
+
+    for n, line in enumerate(data):
+        if re.search(searchPat, line) != None:
+            tmp = parseLinkFromLine(line)
+            #print "tmp [%s]" % tmp
+            links[tmp] = (n, tmp) # (lineNro, URL)
+    if len(links) is 1:
+        return links.values()[0]
+    # In case we got several links, correct one will be
+    # the one that includes word "helpful"
+    if len(links) > 1:
+        for key, item in links.iteritems():
+            if "helpful" in item[1]:
+                return item
+    return None
+
+
+def parseCommentCount(line):
+    nmbrPat = re.compile(r"all (\d|,)*\d* customer")
+
+    if line is None or len(line) < 1:
+        return -1
+
+    tmp = re.search(nmbrPat, line)
+    if tmp != None:
+        tmp = tmp.group(0).replace(",", "")
+        return re.search(r"(\d+)", tmp).group(0)
+    return -1
+
+
+def parsePageCount(data):
+    if data is None or len(data) < 1:
+        return -1
+
+    try:
+        for line in reversed(data):
+            if "<span class=\"paging\">" in line:
+                tmp = line.split("&hellip") # Split from '...'
+                tmp = tmp[1].split(">") # Page number will be in tmp[1]
+                tmp = tmp[1].split("<") # Remove "</a"
+                return tmp[0].replace(",", "")
+    except:
+        return -1
+    return -1
+
+
+def parseLinkFromLine(s):
+    i = 0
+    idx_start = 0
+
+    if s is None or len(s) < 1:
+        return None
+
+    idx_start = s.find("http")
+    if idx_start is -1:
+        return None
+    i = idx_start
+    while i < len(s):
+        if s[i] is '>' or s[i] is '"':
+            return s[idx_start:i]
+        i += 1
+    return s[idx_start:i]
+
+
+def parseComments(data):
+    """Collect comments from a page and return as a list"""
+    comments = []
+    n = 0
+    while n < len(data):
+        line = data[n]
+        # Comment starts
+        if "<!-- BOUNDARY -->" in line:
+            comm = Comment()
+            # Comment section ends with </table>
+            while "</table>" not in line or n < len(data):
+                line = data[n]
+                if "<a name=\"" in line:
+                    #print "Comment\nName: %s, %d" % (type(line), len(line))
+                    comments.append(line)
+                    #comm.name = stripHtmlTags(line)
+                    #print "Comment\nName: %s" % comm.name
+                if "</table>" in line:
+                    break
+                n += 1
+        n += 1
+    return comments
+
+
+def estimatedTimeOfArrival(t, pagesProcessed, pageCount):
+    timeGone = time() - t
+    avg = timeGone/pagesProcessed
+    return float((pageCount - pagesProcessed) * avg)
+
+
+def main():
+    #amazonurl = "http://www.amazon.com/Introduction-Algorithms-Third-Thomas-Cormen/dp/0262033844/ref=sr_1_1?s=books&ie=UTF8&qid=1287355312&sr=1-1"
+    #url = "http://www.amazon.com/Introduction-Algorithms-Third-Thomas-Cormen/dp/0262033844/ref=sr_1_1?s=books&ie=UTF8&qid=1287355312&sr=1-1"
+    #url = """<span class="small"><b><b class="h3color">&rsaquo;</b>&nbsp;<a href="http://www.amazon.com/Introduction-Algorithms-Third-Thomas-Cormen/product-reviews/0262033844/ref=cm_cr_dp_all_recent?ie=UTF8&showViewpoints=1&sortBy=bySubmissionDateDescending" >See all 15 customer reviews...</a></b></span>
+    #"""
+    if len(argv) < 2:
+        amazonurl = "data2.html"
+    else:
+        amazonurl = argv[1]
+
+    data = urlopener(amazonurl)
+    if data is None:
+        print "Zero data"
+        exit(1)
+
+    # Where the comments starts
+    # -> get URL
+    # -> while getNextPageURL
+    # -> parse comments
+    # -> move to next page
+    revStarts = parseReviewsStartLine(data) # Returns (lineNmbr, link)
+    commentsLineNro = int(revStarts[0])
+    cmntTotal = int(parseCommentCount(data[commentsLineNro]))
+
+    if cmntTotal is -1:
+        print "Cannot parse comment count -> malfunction"
+        exit(2)
+    if revStarts is None:
+        print "Cannot determine where the comments starts"
+        exit(3)
+
+    data = urlopener(revStarts[1])
+    comments = []
+    cmntCount = 0
+    pageCount = 0
+    pagesTotal = 0
+    timePassed = time()
+    while getNextPageURL(data):
+        nextPage = getNextPageURL(data)
+        #print getNextPageURL(data)
+        data = urlopener(nextPage)
+        if pageCount  == 0:
+            pagesTotal = int(parsePageCount(data))
+        # PARSE COMMENTS IN HERE
+        comments.extend(parseComments(data))
+        cmntCount = len(comments)
+        print "\rComments: %d/%d   Page: %d/%d   [ETA: %d sec]" % (cmntCount, cmntTotal,\
+                pageCount, pagesTotal, estimatedTimeOfArrival(timePassed,\
+                        pageCount, pagesTotal)),
+        stdout.flush()
+        pageCount += 1
+    # Check whether we have gone through all pages
+    if pageCount < pagesTotal:
+        print "Fetched data not in consistent state. Aborting"
+        exit(5)
+
+
+if __name__ == "__main__":
+    main()
