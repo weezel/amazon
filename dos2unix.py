014981c685413faeb44be9fe69af7630a08e08d7
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2010-11-28 17:52:54 +0100

    Finished dos2unix.py tool

--- dos2unix.py
+++ dos2unix.py
@@ -1,43 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, shutil, sys
+import mimetypes
+import os
+import shutil
+import sys
 
-if len(sys.argv) < 1:
+
+if len(sys.argv) < 2:
     print "Give directory as parameter"
     sys.exit(1)
 
 
-for dpath, dirs, files in os.walk(sys.argv[1]):
-    # Don't visit .git
-    if ".git" in dirs:
-        dirs.remove(".git")
-    for f in files:
-        # Do not touch binary files
-        if ".png" in f or ".pyc" in f:
-            continue
+mimetypes.init()
+# These extensions will be skipped
+mimetypes.add_type("text/properties", ".properties")
+mimetypes.add_type("text/swp", ".swp")
+mimetypes.add_type("text/mf", ".mf")
 
-        if len(dpath) is 1:
-            print "%s" % f
-        else:
-            strpd = dpath.replace("./", "")
-            f = "%s/%s" % (strpd, f)
-            changed = False
-            tmp = []
-            with open(f) as ff:
-                for i in ff:
-                    if "\r\n" in i or "\r\t\n" in i:
-                        changed = True
-                    tmp.append(i.replace("\r\n", "\n").replace("\r\t\n", "\n"))
+
+def visitDir(args, dirname, filenames):
+    global mimetypes
+
+    # Do not process .git directory
+    if ".git" in dirname:
+        return
+    for f in filenames:
+        ff = os.path.join(dirname, f)
+        m = mimetypes.guess_type(ff)[0]
+
+        if os.path.isfile(ff) and \
+                m is not None and \
+                "text" in m:
+            #print "%s  filetype: %s" % (ff, m)
+            try:
+                changed = False
+                outfile = None
+                tmp = []
+
+                with open(ff) as fh:
+                    for i in fh:
+                        if "\r\n" in i or "\r\t\n" in i and changed is False:
+                            changed = True
+                        tmp.append(i.replace("\r\n", "\n").replace("\r\t\n", "\n"))
                 if changed:
-                    try:
-                        outfile = open(ff, "w")
-                        outfile.close()
-                        outfile = open(ff, "a")
-                        for i in tmp:
-                            outfile.write(i)
-                    except:
-                        print "Could not write to %s" % ff
-                    finally:
-                        if outfile is not None:
-                            outfile.close()
+                    print "Modified file: %s" % ff
+                    outfile = open(ff, "w")
+                    outfile.writelines(tmp)
+                    outfile.close()
+            except:
+                print "Could not write file: %s" % os.path.join(dirname, f)
+
+
+def main():
+    os.path.walk(sys.argv[1], visitDir, None)
+
+
+if __name__ == '__main__':
+    main()
+
