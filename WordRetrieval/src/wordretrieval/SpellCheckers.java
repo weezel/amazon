d9566f8c40c2b025e681ea5c289fce9c845d835f
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2011-01-03 15:57:43 +0100

    Damerau addition in SpellCheckers.java is on the way (has few glitches), Commit TfIdf.java (final touch missing)

--- WordRetrieval/src/wordretrieval/SpellCheckers.java
+++ WordRetrieval/src/wordretrieval/SpellCheckers.java
@@ -4,17 +4,72 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/* Add this to levenstein unittest 
+ String1       String2  Dist1  Dist2   Prox1  Prox2
+     hte           hte    0.0    0.0    -0.0   -0.0
+     hte          htne    1.0    1.0    -1.0   -1.0
+     hte          thhe    2.0    2.0    -2.0   -2.0
+     hte           the    2.0    1.0    -2.0   -1.0
+     hte          then    3.0    2.0    -3.0   -2.0
+     hte           The    2.0    2.0    -2.0   -2.0
+     hte           THE    3.0    3.0    -3.0   -3.0
+    htne           hte    1.0    1.0    -1.0   -1.0
+    htne          htne    0.0    0.0    -0.0   -0.0
+    htne          thhe    3.0    2.0    -3.0   -2.0
+    htne           the    2.0    2.0    -2.0   -2.0
+    htne          then    3.0    2.0    -3.0   -2.0
+    htne           The    3.0    3.0    -3.0   -3.0
+    htne           THE    4.0    4.0    -4.0   -4.0
+    thhe           hte    2.0    2.0    -2.0   -2.0
+    thhe          htne    3.0    2.0    -3.0   -2.0
+    thhe          thhe    0.0    0.0    -0.0   -0.0
+    thhe           the    1.0    1.0    -1.0   -1.0
+    thhe          then    2.0    2.0    -2.0   -2.0
+    thhe           The    2.0    2.0    -2.0   -2.0
+    thhe           THE    4.0    4.0    -4.0   -4.0
+     the           hte    2.0    1.0    -2.0   -1.0
+     the          htne    2.0    2.0    -2.0   -2.0
+     the          thhe    1.0    1.0    -1.0   -1.0
+     the           the    0.0    0.0    -0.0   -0.0
+     the          then    1.0    1.0    -1.0   -1.0
+     the           The    1.0    1.0    -1.0   -1.0
+     the           THE    3.0    3.0    -3.0   -3.0
+    then           hte    3.0    2.0    -3.0   -2.0
+    then          htne    3.0    2.0    -3.0   -2.0
+    then          thhe    2.0    2.0    -2.0   -2.0
+    then           the    1.0    1.0    -1.0   -1.0
+    then          then    0.0    0.0    -0.0   -0.0
+    then           The    2.0    2.0    -2.0   -2.0
+    then           THE    4.0    4.0    -4.0   -4.0
+     The           hte    2.0    2.0    -2.0   -2.0
+     The          htne    3.0    3.0    -3.0   -3.0
+     The          thhe    2.0    2.0    -2.0   -2.0
+     The           the    1.0    1.0    -1.0   -1.0
+     The          then    2.0    2.0    -2.0   -2.0
+     The           The    0.0    0.0    -0.0   -0.0
+     The           THE    2.0    2.0    -2.0   -2.0
+     THE           hte    3.0    3.0    -3.0   -3.0
+     THE          htne    4.0    4.0    -4.0   -4.0
+     THE          thhe    4.0    4.0    -4.0   -4.0
+     THE           the    3.0    3.0    -3.0   -3.0
+     THE          then    4.0    4.0    -4.0   -4.0
+     THE           The    2.0    2.0    -2.0   -2.0
+     THE           THE    0.0    0.0    -0.0   -0.0
+*/
+
 public class SpellCheckers
 {
 	/** 
 	 * Converted to Java from Wikipedia's article:
 	 * http://en.wikipedia.org/wiki/Levenshtein_distance
+	 * includes Damerau addition to detect transpositions too
 	 */
 	public static int levensteinDistance(String s1, String s2)
 	{
 		if (s1 == null || s2 == null || s1.length() < 1 || s2.length() < 1)
 			return -1;
 
+		int cost;
 		int[][] resultable = new int[s1.length() + 1][s2.length() + 1];
 
 		for (int i=0; i <= s1.length(); i++)
@@ -23,14 +78,23 @@ public class SpellCheckers
 			resultable[0][j] = j;
 		for (int j=1; j <= s2.length(); j++) {
 			for (int i=1; i <= s1.length(); i++) {
-				if (s1.charAt(i - 1) == s2.charAt(j - 1))
-					resultable[i][j] = resultable[i-1][j-1];
-				else {
+				if (s1.charAt(i-1) == s2.charAt(j-1)) {
+					cost = 0;
+					//resultable[i][j] = resultable[i-1][j-1];
+				} else {
+					cost = 1;
 					resultable[i][j] = Math.min(
 							Math.min(resultable[i-1][j] + 1, // deletion
-								resultable[i][j-1] + 1), // insertion
-							resultable[i-1][j-1] + 1); // substitution
+								resultable[i][j-1] + 1),     // insertion
+							resultable[i-1][j-1] + cost);    // substitution
 				}
+				if (i > 2 && j > 2 &&
+					s1.charAt(i-1) == s2.charAt(j-2) &&
+					s1.charAt(i-2) == s2.charAt(j-1)) {
+					resultable[i][j] = Math.min(resultable[i][j],
+							resultable[i-2][j-2] + cost);	  // transposition
+				}
+
 			} // inner for
 		} // outer for
 		return resultable[s1.length()][s2.length()];
---
+++ WordRetrieval/src/wordretrieval/TfIdf.java
@@ -0,0 +1,150 @@
+package wordretrieval;
+
+//import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class TfIdf
+{
+	/*
+	Consider a document containing 100 words wherein the word cow appears 3
+	times. Following the previously defined formulas, the term frequency
+	(TF) for cow is then (3 / 100) = 0.03.
+	Now, assume we have 10 million documents and cow appears in one thousand of
+	these. Then, the inverse document frequency is calculated as log(10 000 000
+	/ 1 000) = 4. The TF-IDF score is the product of these quantities: 0.03 × 4
+	= 0.12.
+    */
+
+	/*
+	 * Count how many times searhcable word appears in a comment and divide the
+	 * total count of the words in comment by the sum of the found words.
+	 */
+	public static double termFrequencyInDocument(String[] doc, int[] sArea, String s)
+	{
+		int matchCount, spos, epos;
+
+		matchCount = 0;
+		spos = sArea[0];
+		epos = sArea[1];
+
+		for (; spos <= epos; spos++) {
+			if (doc[spos].equals(s))
+				matchCount++;
+		}
+		// TODO: Declare nTimesInComments also
+		//System.out.println("matchCount = '" + matchCount + "'" + ", doc.length=" + doc.length);
+
+		return (double) matchCount / doc.length;
+	}
+
+
+	/*
+	 * Count how many times searhcable word appears in all comments and
+	 * calculate the inverse frequency.
+	 */
+	public static double inverseDocumentFrequency(String[] doc, int[] sArea, int cmtcount, String s)
+	{
+		int foundNtimes, spos, epos;
+
+		foundNtimes = 0;
+		spos = sArea[0];
+		epos = sArea[1];
+
+		assert (foundNtimes > 0) : "Cannot divide with zero! \n\tCaused by foundNtimes: " + foundNtimes;
+
+		if (foundNtimes <= 0)
+			return 0.0;
+
+		return (double) Math.log10(doc.length / foundNtimes);
+	}
+
+	
+	public static double tf_idf(String[] doc, int cmcnt, String searchable)
+	{
+		int[] searchArea;
+		double invtermfreq, termfreq, tfidf_score;
+
+		tfidf_score = 0.0;
+		searchArea = filterByInitials(doc, searchable);
+
+		termfreq = termFrequencyInDocument(doc, searchArea, searchable); // this also declares nTimesInComment(s)
+		System.out.println("TERMFREQ = " + termfreq);
+//FIXME		invtermfreq = inverseDocumentFrequency(cmcnt, nTimesInComments);
+		invtermfreq = inverseDocumentFrequency(doc, searchArea, cmcnt, searchable);
+		System.out.println("INVTERMFREQ = " + invtermfreq);
+
+		if (termfreq == -1)
+			return (0.0);
+
+		tfidf_score = termfreq / invtermfreq;
+
+		return tfidf_score;
+	}
+
+
+	/*
+	 * Reduce search area to the same initial and avoid useless looping.
+	 * @return start and ending position to the document
+	 */
+	public static int[] filterByInitials(String[] doc, String s)
+	{
+		int spos, epos;
+		int[] r = new int[2];
+		char cmpr = s.charAt(0);
+		
+		spos = epos = -1;
+		
+		for (int i = 0; i < doc.length; i++) {
+			/* Rewind to the correct inital position in the array */
+			if (doc[i].charAt(0) == cmpr) {
+				spos = i;
+				while (i < doc.length - 1 && doc[++i].charAt(0) == cmpr);
+				epos = i - 1;
+				break;
+			}
+		}
+		System.out.println("Before, spos: " + spos + ", epos: " + epos);
+
+		/* There is no such initial letter that equals s[0] */
+		if (spos == -1 || epos == -1)
+			return null;
+
+		/* Only count in words that equals with the parameter 's' */
+		for (int i = spos; i <= epos; i++) {
+			if (doc[i].equals(s)) {
+				spos = i;
+				while (i < doc.length - 1 && doc[++i].equals(s));
+				epos = i - 1;
+				break;
+			}
+		}
+		System.out.println("After, spos: " + spos + ", epos: " + epos);
+		
+		r[0] = spos;
+		r[1] = epos;
+
+		return r;
+	}
+	
+
+	public static void main (String[] args)
+	{
+		double jaa;
+		String[] t = {
+			"14sia",
+			"auto", "amiraali", "anjovis",
+			"boredom", "boo", "boo", "boo", "burzum",
+			"klonkku",
+			"jee"
+		};
+
+		for (int i=0; i < t.length; i++)
+			System.out.println(String.format("[%d] = %s", i, t[i]));
+		System.out.println();
+
+		//jaa = termFrequencyInDocument(t, "dukka");
+		jaa = tf_idf(t, t.length, "boo");
+		System.out.println("jaa = " + jaa);
+	}
+
+}
